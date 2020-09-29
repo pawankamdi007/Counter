c663436288b998690ecbabd7478875aa998fa2bb
pawan <pawankamdi02@gmail.com>
2020-09-29 16:35:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+
 
 function App() {
+
+  const[count,setcount]=useState(0);
+  //const[newcount,setnewcount]=useState();
+
+  const plus=()=>{
+   // counter++;
+    setcount(count+1)
+    console.log(count);
+  }
+  const minus=()=>{
+    if(count>0){
+   setcount(count-1);
+    }
+    console.log(count);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+      <h2>Counter</h2>
+        <h1>{count}</h1>
+        <button id="btn" onClick={minus}>Decrement</button>
+        <button id="btn" onClick={plus}>Increment</button>
+        </div>
     </div>
   );
 }
